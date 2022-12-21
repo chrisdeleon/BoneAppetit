b6c2d9b9625ce6c89b00d1a20337619b9d373ae2
Chris de Leon <95385991+chrisdeleon@users.noreply.github.com>
2022-12-20 21:52:39 -0600

    continued work on responsive nav

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
+
+/* This code causes the navbar to disappear as the user scrolls down
+ and reappear as the user scrolls up. */
 var initialPagePosition = window.pageYOffset; // Starting point of page offset in the y-axis. By default it is 0.
 var currentPagePosition = window.pageYOffset; 
-var navbar = document.getElementById('navbar');
+var navUnorderedList = document.getElementById('navbarUl');
 window.onscroll = function () { // Event listener added to the entire window that runs a function when activated.
     currentPagePosition = window.pageYOffset; // Updates the variable to reflect the current position
     if (initialPagePosition > currentPagePosition){
-        navbar.style.top = "0"; // resets the top style so that the navbar appears
+        navUnorderedList.style.top = "0"; // resets the top style so that the navbar appears
     } else {
-        navbar.style.top = "-12vh"; // "moves" the navbar 12vh up so that it appears off screen, giving the disappearing effect
+        navUnorderedList.style.top = "-12vh"; // "moves" the navbar 12vh up so that it appears off screen, giving the disappearing effect
     }
     // This allows for the function to run properly based off currently location, not just initial position
     initialPagePosition = currentPagePosition; 
+}
+
+/* END NAVBAR APPEAR AND DISAPPEAR ON SCROLL */
+
+/* This code toggles the responsive CSS class that makes the items in the navbar appear 
+after the user clicks on the hamburger menu. */
+
+
+var navContainer = document.getElementById('navbarContainer');
+navContainer.addEventListener("click", navbarToggle);
+
+function navbarToggle(){
+    if (navContainer.className == "navbarItem") {
+        navContainer.className += " responsive";
+    } else {
+        navContainer.className = "navbarItem";
+    }
 }
